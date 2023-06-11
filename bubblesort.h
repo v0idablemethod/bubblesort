0f9b7cad1503bdef3e7ee8e639084bef16c28a4d
VoidableMethod <122625617+v0idablemethod@users.noreply.github.com>
2023-06-11 19:38:36 +0200

    Update bubblesort.h

--- bubblesort.h
+++ bubblesort.h
@@ -1,4 +1,8 @@
-#include "helpers.h"
+void swap(int *x, int *y) {
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
 
 void bubbleSort(int *arr, int arrLength) {
     for(int n = 0; n < arrLength - 1; n++) {
